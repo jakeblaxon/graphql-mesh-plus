033a253fda94d14bef2455f8ba3661ca8fdfa382
Blake Jackson <blakejackson@Blakes-MBP.lan>
2020-07-31 23:59:14 -0500

    updated cli package to use refactored runtime

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,21 +1,2 @@
-#!/usr/bin/env node
-var fs = require("fs");
-var nodeEval = require("node-eval");
-
-function readModuleFile(path: string, callback: any) {
-  try {
-    var filename = require.resolve(path);
-    fs.readFile(filename, "utf8", callback);
-  } catch (e) {
-    callback(e);
-  }
-}
-
-var filename = require.resolve("@graphql-mesh/cli")
-readModuleFile(filename, function (err: any, words: string) {
-  words = words.replace(
-    "const runtime = require('@graphql-mesh/runtime');",
-    "const runtime = require('@jakeblaxon-graphql-mesh/runtime');"
-  );
-  module.exports = nodeEval(words, filename);
-});
+#! /usr/bin/env node
+export * from "./bin";
