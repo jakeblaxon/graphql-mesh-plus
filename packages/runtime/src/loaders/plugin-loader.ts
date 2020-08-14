306da9620d423b270b03925ef2e04a991f158d57
Blake Jackson <blakejackson@Blakes-MBP.lan>
2020-08-13 21:00:44 -0500

    removed graphql-mesh/utils dependency

--- packages/runtime/src/loaders/plugin-loader.ts
+++ packages/runtime/src/loaders/plugin-loader.ts
@@ -1,4 +1,4 @@
-import { loadFromModuleExportExpression } from "@graphql-mesh/utils";
+import { isAbsolute, join } from "path";
 import { MeshPlugin, PluginLoader, MeshConfig } from "../types";
 
 export class DefaultPluginLoader implements PluginLoader {
@@ -30,4 +30,34 @@ export function combinePluginLoaders(...loaders: (PluginLoader | undefined)[]): 
       throw new Error("Could not load mesh plugin " + name);
     },
   };
+}
+
+async function loadFromModuleExportExpression(expression: string, defaultExportName?: string) {
+  const [modulePath, exportName = defaultExportName] = expression.split("#");
+  const mod = await tryImport(modulePath);
+  if (exportName === "default" || !exportName) {
+    return mod.default || mod;
+  } else {
+    return mod[exportName] || (mod.default && mod.default[exportName]);
+  }
+}
+
+async function tryImport(modulePath: string) {
+  try {
+    return await import(modulePath);
+  } catch (e1) {
+    if (!isAbsolute(modulePath)) {
+      try {
+        const absoluteModulePath = isAbsolute(modulePath) ? modulePath : join(process.cwd(), modulePath);
+        return await import(absoluteModulePath);
+      } catch (e2) {
+        if (e2.message.includes("Cannot find module")) {
+          throw e1;
+        } else {
+          throw e2;
+        }
+      }
+    }
+    throw e1;
+  }
 }
